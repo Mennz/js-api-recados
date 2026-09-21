dd2d548efc8fcae59456d1fff773d191ae965046
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-21 13:15:25 -0300

    adiciona rota de listar recados

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const PORTA = 3000;
 
 app.use(express.json());
 
+let recados = [
+  { id: 1, texto: "comprar pao" },
+  { id: 2, texto: "estudar express" },
+];
+
 app.get("/", (req, res) => {
   res.send("api de recados no ar");
+});
+
+app.get("/recados", (req, res) => {
+  res.json(recados);
 });
 
 app.listen(PORTA, () => {
